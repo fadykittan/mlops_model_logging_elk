8ecd52361741fd16d3024c535543e0267157a925
fadykittan <fady.kittan@gmail.com>
2025-05-08 17:59:04 +0300

    test_v1

--- main_api.py
+++ main_api.py
@@ -1,18 +1,31 @@
 from flask import Flask, jsonify
 from main import main
+import logging
 
+logging.basicConfig(
+    filename='logs/app.log',             # Log file path
+    filemode='w',                   # 'a' = append, 'w' = overwrite
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    level=logging.INFO              # Log level
+)
+
+logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 
 @app.route('/predict', methods=['GET'])
 def predict():
+    logger.info("access predict, calculating...")
     res = main()
+    logger.info("finish predict")
     return jsonify({"output": str(res)})
 
 @app.route('/', methods=['GET'])
 def home():
+    logger.info("access home page")
     return "Hello World!"
 
 if __name__ == '__main__':
+    logger.info("starting the app...")
     app.run(host="0.0.0.0", port=8080)
 
